f700c95741026b74a9f1e06ecef31a5ffa48282e
Gaurav Kumar <gk107174@gmail.com>
2026-01-30 01:19:39 +0530

    Remove embedd.py

--- src/embedding/embedd.py
+++
@@ -1,9 +0,0 @@
-import getpass
-import os
-
-if not os.environ.get("GOOGLE_API_KEY"):
-    os.environ["GOOGLE_API_KEY"] = getpass.getpass(AIzaSyAs9yU21WMceZVgkkG80jh1AUlE2zfhp6Q)
-
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-
-embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
